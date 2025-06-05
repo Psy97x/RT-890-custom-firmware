86292194e66331edec8a14cbb0d2fd611214ac33
Psy97x <151684891+Psy97x@users.noreply.github.com>
2025-06-05 23:42:58 +0200

    Update helper.h

--- helper/helper.h
+++ helper/helper.h
@@ -14,76 +14,17 @@
  *     limitations under the License.
  */
 
-#ifndef UI_HELPER_H
-#define UI_HELPER_H
+#ifndef HELPER_H
+#define HELPER_H
 
-#include <stdbool.h>
 #include <stdint.h>
 
-enum UI_Icon_t {
-	ICON_LOCK       = 0x0008U,
-	ICON_BELL       = 0x080AU,
-	ICON_SCAN       = 0x120DU,
-	ICON_DUAL_WATCH = 0x1F11U,
-	ICON_BATTERY    = 0x3013U,
-	ICON_VOX        = 0x4318U,
-	ICON_TR         = 0x660FU,
-	ICON_RR         = 0x750FU,
-};
+extern char gShortString[10];
 
-typedef enum UI_Icon_t UI_Icon_t;
-
-void UI_DrawString(uint8_t X, uint8_t Y, const char *String, uint8_t Size);
-void UI_DrawSmallCharacter(uint8_t X, uint8_t Y, char Digit);
-void UI_DrawPercent(uint8_t X, uint8_t Y);
-void UI_DrawDigits(const char *pDigits, uint8_t Vfo);
-void UI_DrawSmallString(uint8_t X, uint8_t Y, const char *String, uint8_t Size);
-void UI_DrawStatusIcon(uint8_t X, UI_Icon_t Icon, bool bDraw, uint16_t Color);
-void UI_DrawRoger(void);
-void UI_DrawVoltage(uint8_t Vfo);
-void UI_DrawVfoFrame(uint8_t Y);
-void UI_DrawName(uint8_t Vfo, const char *pName);
-void UI_DrawExtra(uint8_t Mode, uint8_t gModulationType, uint8_t Vfo);
-void UI_DrawFrequency(uint32_t Frequency, uint8_t Vfo, uint16_t Color);
-void UI_DrawBigDigit(uint8_t X, uint8_t Y, uint8_t Digit);
-void UI_DrawCss(uint8_t CodeType, uint16_t Code, uint8_t Encrypt, bool bMute, uint8_t Vfo);
-void ConvertRssiToDbm(uint16_t Rssi);
-void ConvertRssiToSmeter(uint16_t Rssi);
-void UI_DrawRxDBM(uint8_t Vfo, bool Clear);
-void UI_DrawRxSmeter (uint8_t Vfo, bool Clear);
-void UI_DrawTxPower(bool bIsLow, uint8_t Vfo);
-void UI_DrawChannel(uint16_t Channel, uint8_t Vfo);
-void UI_DrawRX(uint8_t Vfo);
-void UI_DrawDTMF(void);
-void UI_DrawFMFrequency(uint16_t Frequency);
-void UI_DrawFM(void);
-void UI_DrawBitmap(uint8_t X,uint8_t Y, uint8_t H, uint8_t W, const uint8_t *pBitmap);
-void UI_DrawFrame(uint8_t X0, uint8_t X1, uint8_t Y0, uint8_t Y1, uint8_t Thickness, uint16_t Color);
-void UI_DrawDialog(void);
-void UI_DrawBar(uint8_t Level, uint8_t Vfo);
-void UI_DrawSomething(void);
-void UI_DrawMainBitmap(bool bOverride, uint8_t Vfo);
-void UI_DrawSky(void);
-void UI_DrawFrequencyEx(const char *String, uint8_t Vfo, bool bFlag);
-void UI_DrawBootVoltage(uint8_t X, uint8_t Y);
-void UI_DrawDecimal(const char *pInput);
-void UI_DrawMenuPosition(const char *pString);
-void UI_DrawStringSwitchType(void);
-void UI_DrawRadar(void);
-void UI_DrawGolay(void);
-void UI_DrawChannelNumber(const char *pString);
-void UI_DrawBand(void);
-void UI_DrawScanFrequency(uint32_t Frequency);
-void UI_DrawCtdcScan(void);
-void UI_DrawCtcssCode(uint16_t Code);
-void UI_DrawDcsCodeN(uint16_t Code);
-void UI_DrawDTMFString(void);
-void UI_DrawMuteInfo(bool bIs24Bit, uint32_t Golay);
-void UI_DrawNone(void);
-void UI_DrawScan(void);
-#ifdef ENABLE_SCANLIST_DISPLAY
-void UI_DrawScanLists(uint8_t Vfo);
-#endif
+void Int2Ascii(uint32_t Number, uint8_t Size);
+uint16_t TIMER_Calculate(uint16_t Setting);
+void SCREEN_TurnOn(void);
+void STANDBY_BlinkGreen(void);
 
 #endif
 
